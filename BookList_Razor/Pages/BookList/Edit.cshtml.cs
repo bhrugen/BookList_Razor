4934bec57ada8a22434f472368ad23d416a67cf1
Ben Spark <bhrugen1990@gmail.com>
2018-01-01 17:16:42 -0600

    Page Model for Edit Page

--- BookList_Razor/Pages/BookList/Edit.cshtml.cs
+++ BookList_Razor/Pages/BookList/Edit.cshtml.cs
@@ -4,13 +4,48 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BookList_Razor.Model;
 
 namespace BookList_Razor.Pages.BookList
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public void OnGet(int id)
+        {
+            Book = _db.Books.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var bookInDb = _db.Books.Find(Book.Id);
+                bookInDb.ISBN = Book.ISBN;
+                bookInDb.Title = Book.Title;
+                bookInDb.Author = Book.Author;
+                bookInDb.Avaibility = Book.Avaibility;
+                bookInDb.Price = Book.Price;
+
+                await _db.SaveChangesAsync();
+                Message = "Book updated successfuly!";
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
         }
     }
 }
