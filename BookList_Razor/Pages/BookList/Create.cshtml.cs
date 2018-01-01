dec0714037a8843c77df1b27b6031a27e8e1a72b
Ben Spark <bhrugen1990@gmail.com>
2018-01-01 16:54:22 -0600

    Page Model for Create Page

---
+++ BookList_Razor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BookList_Razor.Model;
+
+namespace BookList_Razor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _db.Books.Add(Book);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
